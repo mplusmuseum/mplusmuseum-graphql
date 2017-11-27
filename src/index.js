4e4e7416b1cb7fc1877072ec21f5ee3ce2ccb81d
rnackman <rachel.nackman@gmail.com>
2017-11-27 16:36:08 -0500

    Hook up GraphiQL IDE.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { graphqlExpress } = require('apollo-server-express')
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
 const schema = require('./schema')
 
 const app = express()
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }))
+
+app.use('/graphiql', graphiqlExpress({
+  endpointURL: './graphql',
+}))
 
 const PORT = 3000
 app.listen(PORT, () => {
