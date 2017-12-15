31b3115774903c89f8e57f3cd77a7ad7f9029f3a
rnackman <rachel.nackman@gmail.com>
2017-12-15 14:21:11 -0500

    Update schema to fit data pulling from ES.

--- src/schema/typeDefs.js
+++ src/schema/typeDefs.js
@@ -1,15 +1,54 @@
 const typeDefs = `
   # An artwork in the M+ collection
   type Artwork {
-    # Artwork ID
+    # TMS ID
     id: ID!
-    tmsid: ID
+    # Accession number
     objectnumber: String!
+    # Creation start date
     datebegin: Int
+    # Creation end date (if range)
     dateend: Int
+    # Display date
     dated: String
-    # Artwork title
-    titles: [TranslatedText]!
+    # Object status can be Accessioned Object, [TK] (translated)
+    objectstatus: [TranslatedText]!
+    # Credit lines (translated)
+    creditlines: [TranslatedText]!
+    # Medium listing (translated)
+    mediums: [TranslatedText]!
+    # Object dimensions (translated)
+    dimensions: [TranslatedText]!
+    # Categories assigned to object
+    areacategories: [AreaCategory]!
+    # Makers assigned to object
+    authors: [Maker]!
+    medias: [Media]!
+    # Title (translated)
+    titles: [TranslatedText]
+  }
+
+  type Media {
+    rank: Int!
+    primarydisplay: Int
+    filename: String!
+  }
+
+  type Maker {
+    rank: Int!
+    author: ID!
+    authornameid: ID!
+    nationality: String
+    name: String
+    birthyear_yearformed: Int
+    deathyear: Int
+    roles: [TranslatedText]!
+  }
+
+  type AreaCategory {
+    rank: Int!
+    type: String!
+    areacat: [TranslatedText!]!
   }
 
   type TranslatedText {
