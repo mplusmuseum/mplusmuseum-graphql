87898450b877b6d5dd9f3ac5961a4fcb65b6a0c8
rnackman <rachel.nackman@gmail.com>
2017-12-22 11:34:30 -0600

    Modify query and type resolvers to allow query by artwork ID.

--- src/resolvers/queryResolvers.js
+++ src/resolvers/queryResolvers.js
@@ -1,37 +1,63 @@
-import { getUniqueAuthors, getUniqueMediums } from '../helpers/resolverHelpers'
+import {
+  getUniqueAuthors,
+  getUniqueMediums,
+  getUniqueAreas,
+  getUniqueCategories
+} from '../helpers/resolverHelpers'
 
 const queryResolvers = {
   Query: {
     author: async (root, data, { elasticsearch: { Artworks } }) => {
-      return await getUniqueAuthors(Artworks).find((author) => {
-        if (data.id) return parseInt(author.author) === parseInt(data.id)
-      })
+      if (data.id) {
+        return await getUniqueAuthors(Artworks).find((author) => {
+          return parseInt(author.author) === parseInt(data.id)
+        })
+      }
     },
     artwork: async (root, data, { elasticsearch: { Artworks } }) => {
-      return await Artworks.find((artwork) => {
-        if (data.id) return parseInt(artwork.id) === parseInt(data.id)
-      })
+      if (data.id)
+        return await Artworks.find(artwork =>parseInt(artwork.id) === parseInt(data.id))
     },
     medium: async(root, data, { elasticsearch: { Artworks } }) => {
       return await getUniqueMediums(Artworks).find((medium) => {
         if (data.id) return parseInt(medium.id) === parseInt(data.id)
       })
     },
+    area: async(root, data, { elasticsearch: { Artworks } }) => {
+      return await getUniqueAreas(Artworks).find((area) => {
+        if (data.id) return parseInt(area.id) === parseInt(data.id)
+      })
+    },
     authors: async (root, data, { elasticsearch: { Artworks } }) => {
       return await getUniqueAuthors(Artworks)
     },
     artworks: async (root, data, { elasticsearch: { Artworks } }) => {
-      const unpackedArtworks = Artworks.map((inArtwork) => {
-        let outArtwork = Object.assign({}, inArtwork)
+      if (data.area) {
+        return await Artworks.filter((artwork) => {
+          return artwork.areacategories.find((ac) => {
+            return ac.areacat.find((areacat) => {
+              return areacat.text === data.area
+            })
+          })
+        })
+      }
 
-        outArtwork.authors = inArtwork.authors[0] // Temporary fix to address author array nesting issue in import script.
-
-        return outArtwork
-      })
-      return await unpackedArtworks
+      return await Artworks
     },
     mediums: async (root, data, { elasticsearch: { Artworks } }) => {
       return await getUniqueMediums(Artworks)
+    },
+    areas: async (root, data, { elasticsearch: { Artworks } }) => {
+      if (data.artwork) {
+         return await Artworks.find((artwork) => {
+          return parseInt(data.artwork) === parseInt(artwork.id)
+        }).areacategories.filter((ac) => ac.type.toLowerCase() === 'area')
+      } else {
+        return await getUniqueAreas(Artworks)
+      }
+    },
+    categories: async (root, data, { elasticsearch: { Artworks } }) => {
+      return await getUniqueCategories(Artworks)
     }
   }
 }
--- src/resolvers/typeResolvers.js
+++ src/resolvers/typeResolvers.js
@@ -1,18 +1,33 @@
-import { getUniqueAuthors, getUniqueMediums } from '../helpers/resolverHelpers'
+import hash from 'string-hash'
+import {
+  getUniqueAuthors,
+  getUniqueMediums,
+  getUniqueAreas,
+  getUniqueCategories
+} from '../helpers/resolverHelpers'
 
 const typeResolvers = {
   Artwork: {
     objectNumber: root => root.objectnumber,
-    authors: async (root, data, { elasticsearch: { Artworks } }) => {
-     return await getUniqueAuthors(Artworks)
-      .filter(author => author.artworks.indexOf(parseInt(root.id)) > -1)
+    dateBegin: root => root.datebegin,
+    dateEnd: root => root.dateend,
+    authors: root => root.authors[0],
+    objectStatus: root => root.objectstatus,
+    medium: (root) => {
+      let medium = {}
+      if (root.mediums[0])
+        medium['id'] = hash(root.mediums[0].text)
+        medium['name'] = root.mediums
+      return medium
     },
-    medium: async (root, data, { elasticsearch: { Artworks } }) => {
-      return await getUniqueMediums(Artworks)
-        .filter(medium => medium.artworks.indexOf(parseInt(root.id)) > -1)[0]
-    }
+    // medium: async (root, data, { elasticsearch: { Artworks } }) => {
+    //   return await getUniqueMediums(Artworks)
+    //     .filter(medium => medium.artworks.indexOf(parseInt(root.id)) > -1)[0]
+    // },
+    area: root => root.areacategories.filter((ac) => ac.type === 'Area')
   },
   Author: {
+    id: root => root.author,
     artworks: async (root, data, { elasticsearch: { Artworks } }) => {
       return await root.artworks.map(id => Artworks
         .find(artwork => parseInt(id) === parseInt(artwork.id)))
@@ -30,6 +45,17 @@ const typeResolvers = {
     authors: async (root, data, { elasticsearch: { Artworks } }) => {
       return await getUniqueAuthors(Artworks)
         .filter(author => author.mediums.indexOf(parseInt(root.id)) > -1)
+    }
+  },
+  Area: {
+    artworks: async (root, data, { elasticsearch: { Artworks } }) => {
+      return await root.artworks.map(id => Artworks.find(artwork => parseInt(id) === parseInt(artwork.id)))
+    },
+    name: root => root.areacat
+  },
+  Category: {
+    artworks: async (root, data, { elasticsearch: { Artworks } }) => {
+      return await root.artworks.map(id => Artworks.find(artwork => parseInt(id) === parseInt(artwork.id)))
     }
   }
 }
