c4ba6dd048a1734b2b28164e494912ae4906defd
rnackman <rachel.nackman@gmail.com>
2017-12-22 11:32:14 -0600

    Add helpers for unique areas & categories.

--- src/helpers/resolverHelpers.js
+++ src/helpers/resolverHelpers.js
@@ -7,12 +7,12 @@ export const getUniqueAuthors = (artworks) => {
       const id = author.author
       if (!uniqueAuthors[id]) {
         uniqueAuthors[id] = author
-        uniqueAuthors[id].artworks = []
+        // uniqueAuthors[id].artworks = []
         uniqueAuthors[id].mediums = []
       }
 
-      if (uniqueAuthors[id].artworks.indexOf(artwork.id) < 0)
-        uniqueAuthors[id].artworks.push(artwork.id)
+      // if (uniqueAuthors[id].artworks.indexOf(artwork.id) < 0)
+      //   uniqueAuthors[id].artworks.push(artwork.id)
 
       if (artwork.mediums && artwork.mediums[0].text) {
         const mediumId = hash(artwork.mediums[0].text)
@@ -55,5 +55,61 @@ export const getUniqueMediums = (artworks) => {
 
   return Object.entries(uniqueMediums).map((medium) => {
     return medium[1]
+  })
+}
+
+export const getUniqueAreas = (artworks) => {
+  let uniqueAreas = {}
+
+  artworks.map((artwork) => {
+    if (artwork.areacategories && artwork.areacategories[0].areacat) {
+      artwork.areacategories.map((areacategory) => {
+        if (areacategory.type.toLowerCase() === 'area') {
+          const id = hash(areacategory.areacat[0].text)
+          if (!uniqueAreas[id]) {
+            areacategory['id'] = id
+            areacategory['artworks'] = []
+            areacategory['name'] = areacategory.areacat
+            uniqueAreas[id] = areacategory
+          }
+
+          if (uniqueAreas[id].artworks.indexOf(artwork.id) < 0) {
+            uniqueAreas[id].artworks.push(artwork.id)
+          }
+        }
+      })
+    }
+  })
+
+  return Object.entries(uniqueAreas).map((area) => {
+    return area[1]
+  })
+}
+
+export const getUniqueCategories = (artworks) => {
+  let uniqueCategories = {}
+
+  artworks.map((artwork) => {
+    if (artwork.areacategories && artwork.areacategories[0].areacat) {
+      artwork.areacategories.map((areacategory) => {
+        if (areacategory.type.toLowerCase() === 'category') {
+          const id = hash(areacategory.areacat[0].text)
+          if (!uniqueCategories[id]) {
+            areacategory['id'] = id
+            areacategory['artworks'] = []
+            areacategory['name'] = areacategory.areacat
+            uniqueCategories[id] = areacategory
+          }
+
+          if (uniqueCategories[id].artworks.indexOf(artwork.id) < 0) {
+            uniqueCategories[id].artworks.push(artwork.id)
+          }
+        }
+      })
+    }
+  })
+
+  return Object.entries(uniqueCategories).map((area) => {
+    return area[1]
   })
 }
