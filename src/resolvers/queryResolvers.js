dcfa73c6408c5a96a780aa74302d3f971ce097dd
Jonathan Dahan <hi@jonathan.is>
2017-12-29 14:10:44 -0500

    Add limit param, default to 100 artworks in the query

--- src/resolvers/queryResolvers.js
+++ src/resolvers/queryResolvers.js
@@ -7,42 +7,45 @@ import {
 
 const queryResolvers = {
   Query: {
-    author: async (root, data, { elasticsearch: { Artworks } }) => {
-      if (data.id) {
+    author: async (obj, args, { elasticsearch: { Artworks } }) => {
+      if (args.id) {
         return await getUniqueAuthors(Artworks).find((author) => {
-          return parseInt(author.author) === parseInt(data.id)
+          return parseInt(author.author) === parseInt(args.id)
         })
       }
     },
-    artwork: async (root, data, { elasticsearch: { Artworks } }) => {
-      if (data.id)
-        return await Artworks.find(artwork =>parseInt(artwork.id) === parseInt(data.id))
+    artwork: async (obj, args, { elasticsearch: { Artworks } }) => {
+      if (args.id) {
+        return await Artworks.find((artwork) => {
+          return parseInt(artwork.id) === parseInt(args.id)
+        })
+      }
     },
-    medium: async(root, data, { elasticsearch: { Artworks } }) => {
+    medium: async(obj, args, { elasticsearch: { Artworks } }) => {
       return await getUniqueMediums(Artworks).find((medium) => {
-        if (data.id) return parseInt(medium.id) === parseInt(data.id)
+        if (args.id) return parseInt(medium.id) === parseInt(args.id)
       })
     },
-    area: async(root, data, { elasticsearch: { Artworks } }) => {
+    area: async(obj, args, { elasticsearch: { Artworks } }) => {
       return await getUniqueAreas(Artworks).find((area) => {
-        if (data.id) return parseInt(area.id) === parseInt(data.id)
+        if (args.id) return parseInt(area.id) === parseInt(args.id)
       })
     },
-    authors: async (root, data, { elasticsearch: { Artworks } }) => {
+    authors: async (obj, args, { elasticsearch: { Artworks } }) => {
       return await getUniqueAuthors(Artworks)
     },
-    artworks: async (root, data, { elasticsearch: { Artworks } }) => {
-      if (data.area) {
+    artworks: async (obj, args, { elasticsearch: { Artworks } }) => {
+      if (args.area) {
         return await Artworks.filter((artwork) => {
           return artwork.areacategories.find((ac) => {
             return ac.areacat.find((areacat) => {
-              return areacat.text === data.area
+              return areacat.text === args.area
             })
           })
-        })
+        }).slice(0, args.limit)
       }
 
-      return await Artworks
+      return await Artworks.slice(0, args.limit)
     },
     mediums: async (root, data, { elasticsearch: { Artworks } }) => {
       return await getUniqueMediums(Artworks)
