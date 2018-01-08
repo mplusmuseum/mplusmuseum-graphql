a75bae895a46ae02f12cd1ef29488ccdd4ce8fe1
rnackman <rachel.nackman@gmail.com>
2018-01-08 18:52:30 -0500

    Combine getUniqueArea & Categories.

--- src/helpers/resolverHelpers.js
+++ src/helpers/resolverHelpers.js
@@ -1,25 +1,26 @@
 import hash from 'string-hash'
 
 export const getUniqueMakers = (artworks) => {
-  const makers = artworks.map((artwork) => {
-    return artwork.makers.map((maker) => {
-      if (maker.author)
-        return JSON.stringify({
-          id: parseInt(maker.author),
-          name: maker.name,
-          birthyear_yearformed: maker.birthyear_yearformed,
-          deathyear: maker.deathyear
-        })
-    })
-  })
+  const makers = artworks.map(artwork =>
+    artwork.makers.map(maker => {
+        if (maker.author)
+          return JSON.stringify({
+            id: parseInt(maker.author),
+            name: maker.name,
+            birthyear_yearformed: maker.birthyear_yearformed,
+            deathyear: maker.deathyear
+          })
+      }
+    )
+  )
 
-  const uniqueMakers = new Set([].concat(...makers))
-  return Array.from(uniqueMakers).map(maker => JSON.parse(maker))
+  return Array.from(new Set([].concat(...makers)))
+    .map(maker => JSON.parse(maker))
 }
 
 export const getUniqueMediums = (artworks) => {
   const mediums = []
-  artworks.forEach((artwork) => {
+  artworks.forEach(artwork => {
     const medium = artwork.mediums
     if (medium && medium[0].text) {
       const id = hash(medium[0].text)
@@ -32,62 +33,27 @@ export const getUniqueMediums = (artworks) => {
     }
   })
 
-  const uniqueMediums = new Set([].concat(...mediums))
-  return Array.from(uniqueMediums).map(medium => JSON.parse(medium))
+  return Array.from(new Set([].concat(...mediums)))
+    .map(medium => JSON.parse(medium))
 }
 
-export const getUniqueAreas = (artworks) => {
-  let uniqueAreas = {}
+export const getUniqueAreaCats = (artworks, type) => {
+  let uniqueAreaCats = {}
 
-  artworks.map((artwork) => {
+  artworks.map(artwork => {
     if (artwork.areacategories && artwork.areacategories[0].areacat) {
-      artwork.areacategories.map((areacategory) => {
-        if (areacategory.type.toLowerCase() === 'area') {
-          const id = hash(areacategory.areacat[0].text)
-          if (!uniqueAreas[id]) {
-            areacategory['id'] = id
-            areacategory['artworks'] = []
-            areacategory['name'] = areacategory.areacat
-            uniqueAreas[id] = areacategory
-          }
-
-          if (uniqueAreas[id].artworks.indexOf(artwork.id) < 0) {
-            uniqueAreas[id].artworks.push(artwork.id)
+      artwork.areacategories.map(areacat => {
+        if (areacat.type.toLowerCase() === type.toLowerCase()) {
+          const id = hash(areacat.areacat[0].text)
+          if (!uniqueAreaCats[id]) {
+            areacat.id = id
+            areacat.name = areacat.areacat
+            uniqueAreaCats[id] = areacat
           }
         }
       })
     }
   })
 
-  return Object.entries(uniqueAreas).map((area) => {
-    return area[1]
-  })
-}
-
-export const getUniqueCategories = (artworks) => {
-  let uniqueCategories = {}
-
-  artworks.map((artwork) => {
-    if (artwork.areacategories && artwork.areacategories[0].areacat) {
-      artwork.areacategories.map((areacategory) => {
-        if (areacategory.type.toLowerCase() === 'category') {
-          const id = hash(areacategory.areacat[0].text)
-          if (!uniqueCategories[id]) {
-            areacategory['id'] = id
-            areacategory['artworks'] = []
-            areacategory['name'] = areacategory.areacat
-            uniqueCategories[id] = areacategory
-          }
-
-          if (uniqueCategories[id].artworks.indexOf(artwork.id) < 0) {
-            uniqueCategories[id].artworks.push(artwork.id)
-          }
-        }
-      })
-    }
-  })
-
-  return Object.entries(uniqueCategories).map((area) => {
-    return area[1]
-  })
+  return Object.entries(uniqueAreaCats).map(areacat => areacat[1])
 }
