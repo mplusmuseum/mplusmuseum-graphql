cf7da69517db1370fa71770420cf87203170347f
rnackman <rachel.nackman@gmail.com>
2018-01-08 18:54:42 -0500

    Add support for Maker.mediums.

--- src/resolvers/queryResolvers.js
+++ src/resolvers/queryResolvers.js
@@ -1,58 +1,55 @@
 import {
   getUniqueMakers,
   getUniqueMediums,
-  getUniqueAreas,
-  getUniqueCategories
+  getUniqueAreaCats
 } from '../helpers/resolverHelpers'
 
 const queryResolvers = {
   Query: {
     maker: async (obj, args, { elasticsearch: { Artworks } }) => {
-      if (args.id) {
-        return await getUniqueMakers(Artworks).find((maker) => {
-          return parseInt(maker.id) === parseInt(args.id)
-        })
-      }
+      if (args.id)
+        return await getUniqueMakers(Artworks).find(maker => parseInt(maker.id) === parseInt(args.id))
     },
     artwork: async (obj, args, { elasticsearch: { Artworks } }) => {
-      if (args.id) {
-        return await Artworks.find((artwork) => {
-          return parseInt(artwork.id) === parseInt(args.id)
-        })
-      }
+      if (args.id)
+        return await Artworks.find(artwork => parseInt(artwork.id) === parseInt(args.id))
     },
     medium: async (obj, args, { elasticsearch: { Artworks } }) => {
-      if (args.id) {
-        return await getUniqueMediums(Artworks).find((medium) => {
-          return parseInt(medium.id) === parseInt(args.id)
-        })
-      }
+      if (args.id)
+        return await getUniqueMediums(Artworks)
+          .find(medium => parseInt(medium.id) === parseInt(args.id))
     },
     area: async (obj, args, { elasticsearch: { Artworks } }) => {
-      return await getUniqueAreas(Artworks).find((area) => {
-        if (args.id) return parseInt(area.id) === parseInt(args.id)
-      })
+      if (args.id)
+        return await getUniqueAreaCats(Artworks, 'area')
+          .find(area => parseInt(area.id) === parseInt(args.id))
     },
     makers: async (obj, args, { elasticsearch: { Artworks } }) => {
       return await getUniqueMakers(Artworks)
     },
     artworks: async (obj, args, { elasticsearch: { Artworks } }) => {
       if (args.area) {
-        return await Artworks.filter((artwork) => {
-          return artwork.areacategories.find((ac) => {
-            return ac.areacat.find((areacat) => {
-              return areacat.text === args.area
-            })
-          })
-        }).slice(0, args.limit)
+        return await Artworks.filter(artwork =>
+          artwork.areacategories.find(areacat =>
+            areacat.areacat.find(ac => ac.text === args.area)
+          )
+        ).slice(0, args.limit)
+      }
+
+      if (args.category) {
+        return await Artworks.filter(artwork =>
+          artwork.areacategories.find(areacat =>
+            areacat.areacat.find(ac => ac.text === args.category)
+          )
+        ).slice(0, args.limit)
       }
 
       if (args.maker) {
-        return await Artworks.filter(artwork => {
-          return artwork.makers.find(maker => {
-            return parseInt(maker.author) === parseInt(args.maker)
-          })
-        })
+        return await Artworks.filter(artwork =>
+          artwork.makers.find(maker =>
+            parseInt(maker.author) === parseInt(args.maker)
+          )
+        ).slice(0, args.limit)
       }
 
       if (args.limit)
@@ -61,6 +58,18 @@ const queryResolvers = {
       return await Artworks
     },
     mediums: async (obj, args, { elasticsearch: { Artworks } }) => {
+      if (args.maker) {
+        return await getUniqueMediums(
+          Artworks.filter(artwork =>
+            artwork.mediums && artwork.mediums[0] &&
+            artwork.makers.find(maker => parseInt(maker.author) === parseInt(args.maker))
+          ))
+        .slice(0, args.limit)
+      }
+
+      if (args.limit)
+        return await getUniqueMediums(Artworks).slice(0, args.limit)
+
       return await getUniqueMediums(Artworks)
     },
     areas: async (obj, args, { elasticsearch: { Artworks } }) => {
@@ -69,11 +78,11 @@ const queryResolvers = {
           return parseInt(args.artwork) === parseInt(artwork.id)
         }).areacategories.filter((ac) => ac.type.toLowerCase() === 'area')
       } else {
-        return await getUniqueAreas(Artworks)
+        return await getUniqueAreaCats(Artworks, 'area')
       }
     },
     categories: async (obj, args, { elasticsearch: { Artworks } }) => {
-      return await getUniqueCategories(Artworks)
+      return await getUniqueAreaCats(Artworks, 'category')
     }
   }
 }
