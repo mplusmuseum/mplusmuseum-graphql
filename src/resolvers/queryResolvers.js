ad997b1edb7b6f8bf34ec05aeec70da93236ed0d
Jonathan Dahan <hi@jonathan.is>
2018-01-24 12:22:00 -0500

    fuzzy match for filter

--- src/resolvers/queryResolvers.js
+++ src/resolvers/queryResolvers.js
@@ -11,38 +11,38 @@ import {
 const queryResolvers = {
   Query: {
     maker: async (obj, args, { elasticsearch: { Artworks } }) => {
-      if (args.id) { return await getUniqueMakers(Artworks).find(maker => parseInt(maker.id) === parseInt(args.id)) }
+      if (args.id) { return getUniqueMakers(Artworks).find(maker => parseInt(maker.id) === parseInt(args.id)) }
     },
     artwork: async (obj, args, { elasticsearch: { Artworks } }) => {
-      if (args.id) { return await Artworks.find(artwork => parseInt(artwork.id) === parseInt(args.id)) }
+      if (args.id) { return Artworks.find(artwork => parseInt(artwork.id) === parseInt(args.id)) }
     },
     medium: async (obj, args, { elasticsearch: { Artworks } }) => {
       if (args.id) {
-        return await getUniqueMediums(Artworks)
+        return getUniqueMediums(Artworks)
           .find(medium => parseInt(medium.id) === parseInt(args.id))
       }
     },
     area: async (obj, args, { elasticsearch: { Artworks } }) => {
       if (args.id) {
-        return await getUniqueAreaCats(Artworks, 'area')
+        return getUniqueAreaCats(Artworks, 'area')
           .find(area => parseInt(area.id) === parseInt(args.id))
       }
     },
     category: async (obj, args, { elasticsearch: { Artworks } }) => {
       if (args.id) {
-        return await getUniqueAreaCats(Artworks, 'category')
+        return getUniqueAreaCats(Artworks, 'category')
           .find(category => parseInt(category.id) === parseInt(args.id))
       }
     },
     makers: async (obj, args, { elasticsearch: { Artworks } }) => {
       if (args.artwork) {
-        return await compileMakerRoles(getUniqueMakers(
+        return compileMakerRoles(getUniqueMakers(
           Artworks.filter(artwork =>
             parseInt(artwork.id) === parseInt(args.artwork))))
       }
 
       if (args.medium) {
-        return await getUniqueMakers(
+        return getUniqueMakers(
           Artworks.filter(artwork =>
             artwork.mediums && artwork.mediums[0] && artwork.mediums[0].text &&
             parseInt(hash(artwork.mediums[0].text)) === parseInt(args.medium)
@@ -50,52 +50,53 @@ const queryResolvers = {
         )
       }
 
-      if (args.limit) { return await getUniqueMakers(Artworks).slice(0, args.limit) }
+      if (args.limit) { return getUniqueMakers(Artworks).slice(0, args.limit) }
 
-      return await getUniqueMakers(Artworks)
+      return getUniqueMakers(Artworks)
     },
     artworks: async (obj, args, { elasticsearch: { Artworks } }) => {
       if (args.area) {
-        return await Artworks.filter(artwork =>
+        return Artworks.filter(artwork =>
           artwork.areacategories.find(areacat => {
             if (areacat.type && areacat.type.toLowerCase() === 'area') {
-              return hash(areacat.areacat[0].text) == args.area
+              return hash(areacat.areacat[0].text) === args.area
             }
           }
         ))
       }
 
       if (args.category) {
-        return await Artworks.filter(artwork =>
+        return Artworks.filter(artwork =>
           artwork.areacategories.find(areacat => {
             if (areacat.type && areacat.type.toLowerCase() === 'category') {
-              return hash(areacat.areacat[0].text) == args.area
+              return hash(areacat.areacat[0].text) === args.area
             }
           }
         )).slice(0, args.limit)
       }
 
       if (args.maker) {
-        return await Artworks.filter(artwork =>
+        return Artworks.filter(artwork =>
           artwork.makers.find(maker =>
             parseInt(maker.author) === parseInt(args.maker)
           )
         ).slice(0, args.limit)
       }
 
-      const query = new RegExp(args.filter)
+      const query = new RegExp(`.*${args.filter}.*`)
       console.log(query)
 
       return Artworks
         .filter(({makers, titles, mediums}) => {
-          return searchhash.forValue({makers, titles, mediums}, query).length
+          const found = searchhash.forValue({makers, titles, mediums}, query)
+          return found.length > 0
         })
         .slice(0, args.limit)
     },
 
     mediums: async (obj, args, { elasticsearch: { Artworks } }) => {
       if (args.maker) {
-        return await getUniqueMediums(
+        return getUniqueMediums(
           Artworks.filter(artwork =>
             artwork.mediums && artwork.mediums[0] &&
             artwork.makers.find(maker => parseInt(maker.author) === parseInt(args.maker))
@@ -103,29 +104,29 @@ const queryResolvers = {
         .slice(0, args.limit)
       }
 
-      if (args.limit) { return await getUniqueMediums(Artworks).slice(0, args.limit) }
+      if (args.limit) { return getUniqueMediums(Artworks).slice(0, args.limit) }
 
-      return await getUniqueMediums(Artworks)
+      return getUniqueMediums(Artworks)
     },
     areas: async (obj, args, { elasticsearch: { Artworks } }) => {
       if (args.artwork) {
-        return await Artworks.find((artwork) => {
+        return Artworks.find((artwork) => {
           return parseInt(args.artwork) === parseInt(artwork.id)
         }).areacategories.filter((ac) => ac.type.toLowerCase() === 'area')
       }
 
-      return await getUniqueAreaCats(Artworks, 'area')
+      return getUniqueAreaCats(Artworks, 'area')
     },
     categories: async (obj, args, { elasticsearch: { Artworks } }) => {
       if (args.artwork) {
-        return await Artworks.find((artwork) => {
+        return Artworks.find((artwork) => {
           return parseInt(args.artwork) === parseInt(artwork.id)
         }).areacategories.filter((ac) => ac.type.toLowerCase() === 'category')
       }
 
-      if (args.limit) { return await getUniqueAreaCats(Artworks, 'category').slice(0, args.limit) }
+      if (args.limit) { return getUniqueAreaCats(Artworks, 'category').slice(0, args.limit) }
 
-      return await getUniqueAreaCats(Artworks, 'category')
+      return getUniqueAreaCats(Artworks, 'category')
     }
   }
 }
