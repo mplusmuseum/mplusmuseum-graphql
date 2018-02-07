242622080de1cad575d19b97648b04e9de5aba9b
Micah Walter <micahwalter@users.noreply.github.com>
2018-02-06 20:53:48 -0500

    Update post-deploy.sh

--- ubuntu/post-deploy.sh
+++ ubuntu/post-deploy.sh
@@ -5,6 +5,5 @@ export HOME=/home/ubuntu
 cd /usr/local/mplusmuseum/projects/mplusmuseum-graphql
 
 sudo yarn
-sudo yarn build
 
-pm2 restart "graphql"
+pm2 restart "mplusmuseum-graphql" --update-env
