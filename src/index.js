e1b16cfbdc0b4e50c743aec6dce8aeee5797ad11
revdancatt <github@revdancatt.com>
2018-02-07 16:17:48 +0000

    Load in a simple index page

--- src/index.js
+++ src/index.js
@@ -1,41 +1,67 @@
-if (process.env.NODE_ENV !== 'prod') require('dotenv').load()
+if (process.env.NODE_ENV !== 'prod') require('dotenv').load();
 
-import express from 'express'
-import cors from 'cors'
-import bodyParser from 'body-parser'
-import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const marked = require('marked');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 
-import schema from './schema'
+import schema from './schema';
 
-import connectES from './connectors/elasticsearchConnector'
+import connectES from './connectors/elasticsearchConnector';
 
 const start = async () => {
+  const elasticsearch = await connectES();
 
-  const elasticsearch = await connectES()
+  /*
+   *  Set up bare bones express framework so we can serve a homepage
+   */
+  const app = express();
+  const hbs = exphbs.create({
+    extname: '.html'
+  });
+  app.engine('html', hbs.engine);
+  app.set('view engine', 'html');
+  app.set('views', path.join(__dirname + '/../src/templates'));
 
-  const app = express()
+  app.use(cors());
 
-  app.use(cors())
+  //  The homepage. To keep things super simple(ish) the actual page contents
+  //  is in a markdown file.
+  //  TODO: Sort out the build process so it actually puts the templates
+  //  and markdown into the `build` folder rather than reading from `src`
+  app.get('/', function(req, res) {
+    const templateValues = {};
+    const mdFile = path.join(__dirname + '/../src/templates/index.md');
+    const indexMarkdown = fs.readFileSync(mdFile, 'utf-8');
+    templateValues.content = marked(indexMarkdown);
+    return res.render('index', templateValues);
+  });
 
-  app.use('/graphql',
+  app.use(
+    '/graphql',
     bodyParser.json(),
     graphqlExpress({
       context: { elasticsearch },
       schema
     })
-  )
+  );
 
-  app.use('/api-explorer',
+  app.use(
+    '/api-explorer',
     graphiqlExpress({
-      endpointURL: './graphql',
+      endpointURL: './graphql'
     })
-  )
+  );
 
-  const {GRAPHQL_HOST='localhost', GRAPHQL_PORT=3000} = process.env;
+  const { GRAPHQL_HOST = 'localhost', GRAPHQL_PORT = 3000 } = process.env;
 
   app.listen(GRAPHQL_PORT, GRAPHQL_HOST, () => {
-    console.log(`GraphQL listening on ${GRAPHQL_HOST}:${GRAPHQL_PORT}`)
-  })
-}
+    console.log(`GraphQL listening on ${GRAPHQL_HOST}:${GRAPHQL_PORT}`);
+  });
+};
 
-start()
+start();
---
+++ src/templates/index.md
@@ -0,0 +1,4 @@
+# Welcome to the M+ GraphQL server
+
+You can find the explorer running at: [/api-explorer](/api-explorer)  
+And the API endpoint itself at: [/graphql](/graphql)
