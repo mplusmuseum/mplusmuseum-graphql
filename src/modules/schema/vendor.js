ad6f0a88b098fc390af27a6bb53609026b012082
revdancatt <github@revdancatt.com>
2018-09-13 00:30:17 +0100

    Start of permissions checking

---
+++ src/modules/schema/vendor.js
@@ -0,0 +1,246 @@
+exports.schema = `
+type Query {
+  hello: String
+  objects(
+    page: Int
+    per_page: Int
+    ids: [Int]
+    lang: String = "en"
+    sort: String = "asc"
+    sort_field: String = "id"
+    category: String
+    area: String
+    medium: String
+    displayDate: String
+    beginDate: Int
+    endDate: Int
+    constituent: Int
+    exhibition: Int
+  ): [LevelOneObject]
+  object(
+    id: Int!
+    lang: String = "en"
+  ): LevelOneObject
+
+  constituents(
+    page: Int
+    per_page: Int
+    ids: [Int]
+    lang: String = "en"
+    sort: String = "asc"
+    sort_field: String = "id"
+    gender: String
+    beginDate: Int
+    endDate: Int
+    nationality: String
+  ): [LevelThreeConstituent]
+  constituent(
+    id: Int!
+    lang: String = "en"
+    page: Int
+    per_page: Int
+    category: String
+    area: String
+    medium: String
+  ): LevelOneConstituent
+
+  exhibitions(
+    page: Int
+    per_page: Int = 500
+    ids: [Int]
+    lang: String = "en"
+    sort: String = "asc"
+    sort_field: String = "id"
+    type: String
+    beginDate: Int
+    endDate: Int
+  ): [LevelThreeExhibition]
+  exhibition(
+    id: Int!
+    lang: String = "en"
+    page: Int
+    per_page: Int
+    category: String
+    area: String
+    medium: String
+  ): LevelOneExhibition
+
+  
+  areas(
+    per_page: Int
+    lang: String = "en"
+    sort: String = "asc"
+    sort_field: String = "id"
+  ): [Area]
+  
+  categories(
+    per_page: Int
+    lang: String = "en"
+    sort: String = "asc"
+    sort_field: String = "id"
+  ): [Categories]
+  
+  mediums(
+    per_page: Int
+    lang: String = "en"
+    sort: String = "asc"
+    sort_field: String = "id"
+  ): [Mediums]
+}
+
+type LevelOneObject {
+  id: Int
+  publicAccess: Boolean
+  objectNumber: String
+  sortNumber: String
+  title: String
+  displayDate: String
+  beginDate: Int
+  endDate: Int
+  dimension: String
+  creditLine: String
+  medium: String
+  classification: Classification
+  constituents: [LevelTwoConstituent]
+  exhibitions: ExhibitionsShort
+}
+
+type LevelTwoObject {
+  id: Int
+  publicAccess: Boolean
+  objectNumber: String
+  sortNumber: String
+  title: String
+  displayDate: String
+  beginDate: Int
+  endDate: Int
+  dimension: String
+  creditLine: String
+  medium: String
+  classification: Classification
+  constituents: [LevelThreeConstituent]
+}
+
+type LevelThreeObject {
+  id: Int
+  publicAccess: Boolean
+  objectNumber: String
+  sortNumber: String
+  title: String
+  displayDate: String
+  beginDate: Int
+  endDate: Int
+  dimension: String
+  creditLine: String
+  medium: String
+  classification: Classification
+}
+
+
+type LevelOneConstituent {
+  id: Int
+  publicAccess: Boolean
+  name: String
+  alphaSortName: String
+  displayBio: String
+  gender: String
+  beginDate: Int
+  endDate: Int
+  nationality: String
+  type: String
+  rank: Int
+  roles: [String]
+  objects: [LevelOneObject]
+}
+
+type LevelTwoConstituent {
+  id: Int
+  publicAccess: Boolean
+  name: String
+  alphaSortName: String
+  displayBio: String
+  gender: String
+  beginDate: Int
+  endDate: Int
+  nationality: String
+  type: String
+  rank: Int
+  role: String
+  objects: [LevelTwoObject]
+}
+
+type LevelThreeConstituent {
+  id: Int
+  publicAccess: Boolean
+  name: String
+  alphaSortName: String
+  displayBio: String
+  gender: String
+  beginDate: Int
+  endDate: Int
+  nationality: String
+  type: String
+}
+
+type LevelOneExhibition {
+  id: Int
+  title: String
+  type: String
+  beginDate: String
+  endDate: String
+  venues: [Venue]
+  objects: [LevelOneObject]
+}
+
+type LevelThreeExhibition {
+  id: Int
+  title: String
+  type: String
+  beginDate: String
+  endDate: String
+  venues: [Venue]
+}
+
+type ExhibitionsShort {
+  exhibitions: [ExhibitionShort]
+  labels: [ExhibitionLabels]
+}
+
+type ExhibitionShort {
+  id: Int
+  title: String
+  type: String
+  beginDate: String
+  endDate: String
+  venues: [Venue]
+  section: String
+}
+
+type ExhibitionLabels {
+  purpose: String
+  text: String
+}
+
+
+type Classification {
+  area: String
+  category: String
+}
+type Area {
+  title: String
+  count: Int
+}
+type Categories {
+  title: String
+  count: Int
+}
+type Mediums {
+  title: String
+  count: Int
+}
+type Venue {
+  title: String
+  beginDate: String
+  endDate: String
+}
+`
